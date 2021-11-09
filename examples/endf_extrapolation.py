13b5a22c49f599417d7ddf4199823da67c9e5484
Jacob Schwartz <jacob@jaschwartz.net>
2021-11-08 22:41:27 -0500

    Label axes in endf extrapolation example

--- examples/endf_extrapolation.py
+++ examples/endf_extrapolation.py
@@ -12,12 +12,15 @@ data_name = 'cross_section_dt.csv'
 x, y = load_data_file(data_name)
 lle = LogLogExtrapolation(x, y, linear_extension=True)
 
-newx = np.logspace(-4, 15, 100)
+newx = np.logspace(-2, 10, 1000)
 fig, ax = plt.subplots()
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylim([1e-99, 1e1])
 ax.plot(x, y)
 ax.plot(newx, lle(newx), ls='dashed')
+
+ax.set_xlabel('Beam-target energy/eV')
+ax.set_ylabel('Cross section/barns')
 
 plt.show()
