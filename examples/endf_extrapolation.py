9d2d3464f3e5cb87e3c0c916d8990b4bfe454fcf
Jacob Schwartz <jacob@jaschwartz.net>
2022-01-04 20:12:14 -0500

    Add some new data for rarer reactions

--- examples/endf_extrapolation.py
+++ examples/endf_extrapolation.py
@@ -8,19 +8,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 default_data_dir = 'fusrate.data'
-data_name = 'cross_section_dt.csv'
-x, y = load_data_file(data_name)
-lle = LogLogExtrapolation(x, y, linear_extension=True)
 
-newx = np.logspace(-2, 10, 1000)
 fig, ax = plt.subplots()
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylim([1e-99, 1e1])
-ax.plot(x, y)
-ax.plot(newx, lle(newx), ls='dashed')
-
 ax.set_xlabel('Beam-target energy/eV')
 ax.set_ylabel('Cross section/barns')
 
+def plot_some_data(name):
+    x, y = load_data_file(name)
+    lle = LogLogExtrapolation(x, y, linear_extension=True)
+
+    newx = np.logspace(-2, 10, 1000)
+    ax.plot(x, y)
+    ax.plot(newx, lle(newx), ls='dashed')
+
+plot_some_data('cross_section_h(h,2p)a.csv')
+plot_some_data('cross_section_t(t,2n)a.csv')
+
+ax.set_ylim([1e-9, 1e1])
 plt.show()
