3b82e8493e33a2bb4ec2277e810d075eba8b524d
Jacob Schwartz <jacob@jaschwartz.net>
2022-03-18 19:00:22 -0400

    Rename variables in reactivity example

--- examples/halebosch_reactivity.py
+++ examples/halebosch_reactivity.py
@@ -7,20 +7,20 @@ line_styles = ['solid', '-.', 'solid','dashed',]
 
 fig, ax = plt.subplots(1, 2)
 
-def plot_cs(reaction):
-    cs = HaleBoschReactivity(reaction)
-    e_range = cs.prescribed_range()
+def plot_sv(reaction):
+    sv = HaleBoschReactivity(reaction)
+    e_range = sv.prescribed_range()
 
     e = np.logspace(*np.log10(e_range), 100)
 
     x_hb = e
-    y_hb = cs.reactivity(e)/1e6
+    y_hb = sv.reactivity(e)/1e6
     ax[0].plot(x_hb, y_hb, label=reaction, ls=line_styles[i])
     ax[1].plot(x_hb, y_hb, label=reaction, ls=line_styles[i])
 
 reactions = ['T(d,n)⁴He', '³He(d,p)⁴He', 'D(d,n)³He', 'D(d,p)T']
 for i, reaction in enumerate(reactions):
-    plot_cs(reaction)
+    plot_sv(reaction)
 
 ax[0].set_xscale('log')
 ax[0].set_yscale('log')
@@ -29,6 +29,7 @@ ax[0].set_xlim([0.1, 2e2])
 ax[0].set_ylim([1e-38,1e-18])
 ax[0].set_xlabel('Temperature/keV')
 ax[0].set_ylabel('Reactivity/ (m³/s)')
+ax[0].set_title('Wide-range plot')
 
 ax[1].set_xscale('log')
 ax[1].set_yscale('log')
@@ -37,6 +38,7 @@ ax[1].set_xlim([1e1,2e2])
 ax[1].set_ylim([1e-25,1e-21])
 ax[1].set_xlabel('Temperature/keV')
 ax[1].set_ylabel('Reactivity (m³/s)')
+ax[1].set_title('Zoom-in on peaks')
 
 ax[0].legend()
 plt.show()
