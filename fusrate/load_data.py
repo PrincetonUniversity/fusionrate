302493067f19e296135469b8b8517ed49bfb9bee
Jacob Schwartz <jacob@jaschwartz.net>
2022-04-16 18:05:54 -0500

    Add ability to load cross section data

--- fusrate/load_data.py
+++ fusrate/load_data.py
@@ -1,8 +1,13 @@
+from fusrate.reactionnames import reaction_name_simplify
+from fusrate.reactionnames import reaction_name_to_endf
+
 from importlib import resources
 
 import numpy as np
 
 DEFAULT_DATA_DIR = 'fusrate.data'
+CROSS_SECTION_PREFIX = 'cross_section_'
+CROSS_SECTION_FILETYPE = '.csv'
 
 def locate_data_file(dname):
     if resources.is_resource(DEFAULT_DATA_DIR, dname):
@@ -17,8 +22,17 @@ def load_data_file(dname):
     path = locate_data_file(dname)
     return np.loadtxt(path, delimiter=',').T
 
+def cross_section_filename(canonical_reaction_name):
+    s = reaction_name_simplify(canonical_reaction_name)
+    s = reaction_name_to_endf(s)
+    return CROSS_SECTION_PREFIX + s + CROSS_SECTION_FILETYPE
+
+def cross_section_data(canonical_reaction_name):
+    filename = cross_section_filename(canonical_reaction_name)
+    return load_data_file(filename)
 
 if __name__=='__main__':
-    data_name = 'cross_section_t(d,n)a.csv'
-    print(load_data_file(data_name).T)
+    from reactionnames import DT_NAME
+
+    print(cross_section_data(DDHE3_NAME).T)
 
