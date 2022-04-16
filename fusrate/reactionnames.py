526193314a7b4d9e3b7922b99de79c4ea76dbc1c
Jacob Schwartz <jacob@jaschwartz.net>
2022-04-16 18:05:19 -0500

    Add further reaction names
    These are not yet fully tested-out though

--- fusrate/reactionnames.py
+++ fusrate/reactionnames.py
@@ -6,6 +6,13 @@ DDHE3_NAME = 'D(d,n)³He'
 PLI6_NAME = '⁶Li(p,α)³He'
 PB11_NAME = '¹¹B(p,α)2⁴He'
 
+TT_NAME = 'T(t,2n)⁴He'
+HT_NAME = '³He(t,pn)⁴He'
+HH_NAME = '³He(h,2p)⁴He'
+DLI6A_NAME = '⁶Li(d,α)⁴He'
+DLI6N_NAME = '⁶Li(d,n)⁷Be'
+DLI6P_NAME = '⁶Li(d,p)⁷Li'
+
 ALL_REACTIONS = [
     DT_NAME, DHE3_NAME, DDT_NAME, DDHE3_NAME, PLI6_NAME, PB11_NAME
 ]
@@ -40,11 +47,20 @@ def reaction_name_simplify(reaction_name_raw):
     s = s.replace('²', '2')
     s = s.replace('³', '3')
     s = s.replace('⁴', '4')
+    s = s.replace('⁶', '6')
+    s = s.replace('⁷', '7')
     s = s.replace('->', '→')
     s = s.replace('-', '+')
     s = s.replace('α', 'a')
     return s
 
+def reaction_name_to_endf(canonical_reaction_name):
+    s = canonical_reaction_name
+    s = s.replace('4He', 'a')
+    s = s.replace('3He', 'h')
+    s = s.replace('T', 't')
+    s = s.replace('D', 'd')
+    return s
 
 def proton_boron_name_resolver(reaction_raw_name):
     NAMES = [
