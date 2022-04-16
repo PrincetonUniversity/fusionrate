9dce293b0220e5963bf4bde35e8b22051791a207
Jacob Schwartz <jacob@jaschwartz.net>
2022-04-16 18:06:09 -0500

    Simplfy the ENDF extrapolation example
    This avoids having to specify the filename.

--- examples/endf_extrapolation.py
+++ examples/endf_extrapolation.py
@@ -1,7 +1,7 @@
 # Here I show how the cross section extrapolation works
 # It is linear in log-log space
 
-from fusrate.load_data import load_data_file
+from fusrate.load_data import cross_section_data
 from fusrate.endf import LogLogExtrapolation
 
 import numpy as np
@@ -14,8 +14,8 @@ ax.set_ylim([1e-59, 1e1])
 ax.set_xlabel('Beam-target energy/eV')
 ax.set_ylabel('Cross section/barns')
 
-def plot_some_data(name, label=None):
-    x, y = load_data_file(name)
+def plot_some_data(label):
+    x, y = cross_section_data(label)
     lle = LogLogExtrapolation(x, y, linear_extension=True)
 
     newx = np.logspace(-1, 8, 1000)
@@ -23,16 +23,16 @@ def plot_some_data(name, label=None):
     c = p[0].get_color()
     ax.plot(newx, lle(newx), ls='dashed', color=c)
 
-plot_some_data('cross_section_t(d,n)a.csv', label='t(d,n)α')
-plot_some_data('cross_section_d(d,n)h.csv', label='d(d,n)h')
-plot_some_data('cross_section_d(d,p)t.csv', label='d(d,p)t')
-plot_some_data('cross_section_h(d,p)a.csv', label='h(d,p)α')
-plot_some_data('cross_section_t(t,2n)a.csv', label='t(t,2n)α')
-plot_some_data('cross_section_h(t,pn)a.csv', label='h(t,pn)α')
-plot_some_data('cross_section_h(h,2p)a.csv', label='h(h,2p)α')
-plot_some_data('cross_section_6Li(d,a)a.csv', label='⁶Li(d,α)α')
-plot_some_data('cross_section_6Li(d,n)7Be.csv', label='⁶Li(d,n)⁷Be')
-plot_some_data('cross_section_6Li(d,p)7Li.csv', label='⁶Li(d,p)⁷Li')
+plot_some_data('t(d,n)α')
+plot_some_data('d(d,n)h')
+plot_some_data('d(d,p)t')
+plot_some_data('h(d,p)α')
+plot_some_data('t(t,2n)α')
+plot_some_data('h(t,pn)α')
+plot_some_data('h(h,2p)α')
+plot_some_data('⁶Li(d,α)α')
+plot_some_data('⁶Li(d,n)⁷Be')
+plot_some_data('⁶Li(d,p)⁷Li')
 
 ax.legend()
 plt.show()
