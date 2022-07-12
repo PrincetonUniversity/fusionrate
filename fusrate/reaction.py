dbee2b3f1a086f572564977685a922d706948fc1
Jacob Schwartz <jacob@jaschwartz.net>
2022-07-11 21:56:53 -0400

    Add endf cross sections and reactivity to Reaction

--- fusrate/reaction.py
+++ fusrate/reaction.py
@@ -1,7 +1,11 @@
-from boschhale import BoschHaleCrossSection
-from boschhale import BoschHaleReactivity
+from fusrate.boschhale import BoschHaleCrossSection
+from fusrate.boschhale import BoschHaleReactivity
 
-from ion_data import ion_mass
+from fusrate.endf import ENDFCrossSection
+
+from fusrate.ratecoefficient import RateCoefficientIntegratorMaxwellian
+
+from fusrate.ion_data import ion_mass
 
 import fusrate.reactionnames as rn
 
@@ -36,8 +40,7 @@ class ReactionCore:
 
 
 class Reaction:
-    r"""
-    """
+    r"""Main reaction class"""
 
     def __init__(self, name):
         # try bh name resolver. If that fails,
@@ -46,15 +49,30 @@ class Reaction:
         self.rcore = ReactionCore(name)
         name = self.rcore.canonical_name()
 
-        self.cross_analytic_call = self._no_reactivity_analytic
-        if name in BoschHaleCrossSection.provides_reactions():
+        self.cross_analytic_call = self._no_cross_analytic
+        self.reactivity_analytic_call = self._no_reactivity_analytic
+
+        # the Bosch paper is the only source of analytic fits
+
+        self.has_cross_section_analytic_fit = (
+            name in BoschHaleCrossSection.provides_reactions()
+        )
+        if self.has_cross_section_analytic_fit:
             self.bh_cross = BoschHaleCrossSection(name)
             self.cross_analytic_call = self.bh_cross.cross_section
 
-        self.reactivity_analytic_call = self._no_reactivity_analytic
-        if name in BoschHaleReactivity.provides_reactions():
+        self.has_reactivity_analytic_fit = (
+            name in BoschHaleReactivity.provides_reactions()
+        )
+        if self.has_reactivity_analytic_fit:
             self.bh_react = BoschHaleReactivity(name)
             self.reactivity_analytic_call = self.bh_react.reactivity
+
+        self.cross_sec_interpolator = ENDFCrossSection(name).cross_section
+
+        self.reactivity_integrator = RateCoefficientIntegratorMaxwellian(
+            self.rcore, self.cross_sec_interpolator
+        ).reactivity
 
     def canonical_name(self):
         return self.rcore.canonical_name()
@@ -68,8 +86,21 @@ class Reaction:
     def beam_target_to_com_factor(self):
         return self.rcore.beam_target_to_com_factor()
 
+    ### cross section functions
+
     def cross_section(self, e):
-        return self.cross_analytic_call(e)
+        r"""Look up the interpolated cross section from ENDF data
+
+        Parameters
+        ----------
+        e : array_like,
+          energies in keV
+
+        Returns
+        -------
+        Cross sections in mb
+        """
+        return self.cross_sec_interpolator(e)
 
     def cross_section_d(self, e):
         pass
@@ -77,10 +108,28 @@ class Reaction:
     def cross_section_analytic_fit(self, e):
         return self.cross_analytic_call(e)
 
+    def _no_cross_analytic(self, *T, **kwargs):
+        r"""There is no implemented analytic formation to the reactivity.
+
+        Please do not call the function reactivity_analytic, or it will throw
+        an error.
+        """
+        raise NotImplementedError(
+            f"There is no implemented analytic reactivity"
+            f" function for {self.canonical_name()}."
+        )
+
+    ### reactivity functions
+
     def reactivity(self, T):
         pass
 
+    def reactivity_integration(self, T):
+        r"""Perform an integration"""
+        return self.reactivity_integrator(T)
+
     def reactivity_analytic_fit(self, T):
+        r"""Use the analytic fit"""
         return self.reactivity_analytic_call(T)
 
     def reactivity_d(self, T):
@@ -92,14 +141,19 @@ class Reaction:
         Please do not call the function reactivity_analytic, or it will throw
         an error.
         """
-        raise NotImplementedError(f"There is no implemented analytic reactivity"
-            f" function for {self.canonical_name()}.")
+        raise NotImplementedError(
+            f"There is no implemented analytic reactivity"
+            f" function for {self.canonical_name()}."
+        )
 
 
 if __name__ == "__main__":
     import numpy as np
+
     r = Reaction("D+T")
-    s = r.cross_section_analytic(np.array([1,2,3]))
+    ts = np.array([10, 20, 30])
+    s = r.reactivity_integration(ts)
+    print(s)
+    s = r.reactivity_analytic_fit(ts)
     print(s)
 
-    # do the thing
