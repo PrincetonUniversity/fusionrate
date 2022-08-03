b8b41bc5325006395d20d2036261623acb10c145
Jacob Schwartz <jacob@jaschwartz.net>
2022-08-02 23:23:11 -0400

    Make distributions stop being an enum
    This got confusing because enum's can't be compared to strings.

--- fusrate/constants.py
+++ fusrate/constants.py
@@ -2,11 +2,9 @@ atomic_mass_unit = 1.66054e-27  # kilograms
 kiloelectronvolt = 1.60218e-16  # Joules
 millibarn_meters_squared_to_cubic_centimeter = 1e-25
 
-from enum import Enum
-
 PROJECT = "fusrate"
 
-class Distributions(Enum):
+class Distributions:
     # Two ions with the same temperature and isotropic
     MAXW = "Maxwellian"
     BIMAXW = "BiMaxwellian"
