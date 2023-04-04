0a6efae690c6a242528d9f3e716f9afbec8092b8
Jacob Schwartz <jacob@jaschwartz.net>
2023-04-04 13:06:28 -0400

    Add new reaction name parser.

--- fusionrate/reactionnames.py
+++ fusionrate/reactionnames.py
@@ -1,3 +1,24 @@
+from enum import Enum
+from collections import Counter
+import re
+
+# names must be python identifiers
+Particles = Enum(
+    "Particles",
+    [
+        "n",
+        "H1",
+        "H2",
+        "H3",
+        "He3",
+        "He4",
+        "Li6",
+        "Li7",
+        "Be7",
+        "B11",
+    ],
+)
+
 DT_NAME = "T(d,n)⁴He"
 DHE3_NAME = "³He(d,p)⁴He"
 DDT_NAME = "D(d,p)T"
@@ -19,14 +40,210 @@ ALL_REACTIONS = [
     DDT_NAME,
     DDHE3_NAME,
     PLI6_NAME,
-#   PB11_NAME,
-#   TT_NAME,
-#   HT_NAME,
-#   HH_NAME,
-#   DLI6A_NAME,
-#   DLI6N_NAME,
-#   DLI6P_NAME,
+    PB11_NAME,
+    #   TT_NAME,
+    #   HT_NAME,
+    #   HH_NAME,
+    #   DLI6A_NAME,
+    #   DLI6N_NAME,
+    #   DLI6P_NAME,
 ]
+# missing:
+# 'D + Li-6 --> He-3 + He-4 + n'
+# 'He-3 + T --> He-4 + p + n'
+
+# could make this nicer
+# no 3He because that would be ambiguous with 3 He-4
+ION_SYNONYMS = {
+    Particles.n: ("n-1", "n"),
+    Particles.H1: ("H-1", "¹H", "H", "p"),
+    Particles.H2: ("H-2", "²H", "D", "d"),
+    Particles.H3: ("H-3", "³H", "T", "t"),
+    Particles.He3: ("He-3", "³He", "h"),
+    Particles.He4: ("He-4", "⁴He", "a", "α"),
+    Particles.Li6: ("Li-6", "⁶Li", "6Li"),
+    Particles.Li7: ("Li-7", "⁷Li", "7Li"),
+    Particles.Be7: ("Be-7", "⁷Be", "7Be", "Be"),
+    Particles.B11: ("B-11", "¹¹B", "11B", "B"),
+}
+
+PARTICLE_LOOKUP = dict()
+
+for k, v in ION_SYNONYMS.items():
+    for result in v:
+        PARTICLE_LOOKUP[result] = k
+
+
+def _determine_particle(s: str):
+    p = PARTICLE_LOOKUP.get(s)
+    if p is None:
+        raise ValueError(
+            f"""
+            The particle '{s}' was not found in the lookup table.
+            Valid particles are {sorted(set(PARTICLE_LOOKUP.keys()))}."""
+        )
+    return p
+
+
+def _bag(*particles):
+    counter = Counter(particles)
+    return frozenset(counter.items())
+
+
+# unambiguous
+DT_REACTANTS = _bag(Particles.H2, Particles.H3)
+DHE3_REACTANTS = _bag(Particles.H2, Particles.He3)
+PLI6_REACTANTS = _bag(Particles.H1, Particles.Li6)
+PB11_REACTANTS = _bag(Particles.H1, Particles.B11)
+TT_REACTANTS = _bag(Particles.H3, Particles.H3)
+
+# ambiguous
+DD_REACTANTS = _bag(Particles.H2, Particles.H2)
+HH_REACTANTS = _bag(Particles.He3, Particles.He3)
+HT_REACTANTS = _bag(Particles.H3, Particles.He3)
+DLI6_REACTANTS = _bag(Particles.H2, Particles.Li6)
+HLI6_REACTANTS = _bag(Particles.He3, Particles.Li6)
+
+_REACTIONS = {
+    (DT_REACTANTS, _bag(Particles.n, Particles.He4)): DT_NAME,
+    (DHE3_REACTANTS, _bag(Particles.H1, Particles.He4)): DHE3_NAME,
+    (PLI6_REACTANTS, _bag(Particles.He3, Particles.He4)): PLI6_NAME,
+    (
+        PB11_REACTANTS,
+        _bag(Particles.He4, Particles.He4, Particles.He4),
+    ): PB11_NAME,
+    (TT_REACTANTS, _bag(Particles.He4, Particles.n, Particles.n)): TT_NAME,
+    (HH_REACTANTS, _bag(Particles.He4, Particles.H1, Particles.H1)): HH_NAME,
+    (DD_REACTANTS, _bag(Particles.H3, Particles.H1)): DDT_NAME,
+    (DD_REACTANTS, _bag(Particles.He3, Particles.n)): DDHE3_NAME,
+    (HT_REACTANTS, _bag(Particles.He4, Particles.n, Particles.H1)): HT_NAME,
+    (DLI6_REACTANTS, _bag(Particles.He4, Particles.He4)): DLI6A_NAME,
+    (DLI6_REACTANTS, _bag(Particles.n, Particles.Be7)): DLI6N_NAME,
+    (DLI6_REACTANTS, _bag(Particles.H1, Particles.Li7)): DLI6P_NAME,
+}
+
+
+def _generate_single_branch_list(reactions_lookup: dict):
+    """Get reactions that have only have one possible set of products"""
+    all_reactions = list(reactions_lookup.keys())
+    all_reactants = [r[0] for r in all_reactions]
+    with_unique_reactants = {}
+    for r in all_reactions:
+        reactants, products = r
+        if all_reactants.count(reactants) == 1:
+            with_unique_reactants[reactants] = reactions_lookup[r]
+    return with_unique_reactants
+
+
+_REACTIONS_WITH_UNIQUE_PRODUCTS = _generate_single_branch_list(_REACTIONS)
+
+
+def _to_particle(s: str):
+    s = s.strip()
+    particle = PARTICLE_LOOKUP.get(s)
+    if particle is None:
+        raise ValueError(
+            f"""
+            The particle '{s}' was not found in the lookup table.
+            Valid particles are {sorted(set(PARTICLE_LOOKUP.keys()))}."""
+        )
+    return particle
+
+
+def _normalize_reaction_separators(s: str):
+    s = re.sub(r"-+>", "→", s)
+    return s
+
+
+def _count_reaction_separators(s: str):
+    s = _normalize_reaction_separators(s)
+    separator = "→"
+    return s.count(separator)
+
+
+def _joinparticles(particles: list):
+    return "+".join(particles)
+
+
+def _splitparticles(s: str):
+    return s.split("+")
+
+
+def _validate_reaction_string(s: str):
+    number_of_separators = _count_reaction_separators(s)
+    if number_of_separators not in (0, 1):
+        raise ValueError(
+            f"""Reaction string '{s}'
+           has more than one reactant -> product separator.
+           Only one instance of → or -> is allowed."""
+        )
+
+
+def _multiply_particles(reaction_string):
+    """Turn '2 ³He' to '³He + ³He'"""
+    # This pattern is a bit of a hack.
+    # It purposefully places 'H' at the end since it's a prefix of many of the
+    # other strings. Other than that, only 'n' is a prefix of 'n-1'.
+    searchable_particles = [
+        "n-1",
+        "n",
+        "p",
+        "α",
+        "h",
+        "D",
+        "T",
+        "He-3",
+        "³He",
+        "He-4",
+        "⁴He",
+        "H-1",
+        "H-2",
+        "H-3",
+        "H",
+    ]
+    pattern = (
+        r"(?P<coeff>2|3)\s*(?P<particle>"
+        + "|".join(searchable_particles)
+        + ")"
+    )
+    matches = re.match(pattern, reaction_string)
+    if matches:
+        coeff = int(matches.group("coeff"))
+        particle = matches.group("particle")
+        output_components = [particle] * coeff
+        return _joinparticles(output_components)
+    else:
+        # If the input string doesn't match the pattern, return original string
+        return reaction_string
+
+
+def _expand_particle_description(s: str):
+    s1 = _splitparticles(s)
+    s2 = [_multiply_particles(s) for s in s1]
+    return _joinparticles(s2)
+
+
+def _parse_reactants(s: str):
+    s_expanded = _expand_particle_description(s)
+    particles = _splitparticles(s_expanded)
+    if len(particles) != 2:
+        raise ValueError(
+            f"""The reactant description '{s}' must specify exactly two
+            particles."""
+        )
+    return _bag(*map(_to_particle, particles))
+
+
+def _parse_products(s: str):
+    s_expanded = _expand_particle_description(s)
+    particles = _splitparticles(s_expanded)
+    num_particles = len(particles)
+    if num_particles < 2 or num_particles > 3:
+        raise ValueError(
+            f"""The product description '{s}' must specify
+            either two or three particles. {num_particles} were detected."""
+        )
+    return _bag(*map(_to_particle, particles))
 
 
 def target_species(s: str):
@@ -128,7 +345,33 @@ def reactants(s: str):
     return beam, target
 
 
-def name_resolver(reaction_raw_name: str):
+def _name_parser(s: str):
+    count = _count_reaction_separators(s)
+    if count > 1:
+        raise ValueError(
+            f"""Reaction string '{s}'
+           has more than one reactant -> product separator.
+           Only one instance of → or -> is allowed."""
+        )
+
+    if count == 0:
+        reactants = _parse_reactants(s)
+        canonical_name = _REACTIONS_WITH_UNIQUE_PRODUCTS.get(reactants)
+
+    else:
+        s = _normalize_reaction_separators(s)
+        reactants, products = s.split("→")
+
+        reactants_description = _parse_reactants(reactants)
+        products_description = _parse_products(products)
+        reaction_description = (reactants_description, products_description)
+
+        canonical_name = _REACTIONS.get(reaction_description)
+
+    return canonical_name
+
+
+def _extra_name_resolver(reaction_raw_name: str):
     r"""Recognize a canonical fusion reaction
 
     The name resolver aims to recognize the common fusion reactions
@@ -145,16 +388,8 @@ def name_resolver(reaction_raw_name: str):
     >>> name_resolver("DT")
     'T(d,n)⁴He'
 
-    >>> name_resolver("D+T→α+n")
-    'T(d,n)⁴He'
-
     >>> name_resolver("D+3He")
     '³He(d,p)T'
-
-    Notes
-    -----
-    This could be implemented in a better way by parsing the
-    reactants and products rather than doing a set of string replacements.
     """
     canonical_name = None
     if canonical_name is None:
@@ -175,15 +410,49 @@ def name_resolver(reaction_raw_name: str):
         except ValueError:
             pass
 
+    return canonical_name
+
+
+def name_resolver(s: str):
+    r"""Recognize a canonical fusion reaction
+
+    The name resolver aims to recognize the common fusion reactions
+    from a string, in a reasonable number of unambiguous formats.
+
+    Parameters
+    ----------
+    reaction_raw_name: string
+        The name resolver aims to recognize the common fusion reactions
+        from a string, in a reasonable number of unambiguous formats.
+
+    Examples
+    --------
+    >>> name_resolver("DT")
+    'T(d,n)⁴He'
+
+    >>> name_resolver("D+T→α+n")
+    'T(d,n)⁴He'
+
+    >>> name_resolver("D + He-3")
+    '³He(d,p)T'
+    """
+    if s in ALL_REACTIONS:
+        return s
+
+    canonical_name = _extra_name_resolver(s)
     if canonical_name is not None:
         return canonical_name
-    else:
-        raise ValueError(
-            f"""
-        The reaction name resolver could not determine the reaction
-        {reaction_raw_name}. The best options are {ALL_REACTIONS}.
-        """
-        )
+
+    canonical_name = _name_parser(s)
+    if canonical_name is not None:
+        return canonical_name
+
+    raise ValueError(
+        f"""
+    The reaction name resolver could not determine the reaction
+    {reaction_raw_name}. The best options are {ALL_REACTIONS}.
+    """
+    )
 
 
 def reaction_name_simplify(reaction_name_raw: str):
@@ -221,11 +490,6 @@ def proton_boron_name_resolver(reaction_raw_name):
         PB11_NAME,
         "pB",
         "pB11",
-        "p+B",
-        "p+B11",
-        "p+11B",
-        "p+11B→3α",
-        "p+11B→3 ⁴He",
     ]
 
     reaction_name = reaction_name_simplify(reaction_raw_name)
@@ -244,22 +508,7 @@ def proton_boron_name_resolver(reaction_raw_name):
 
 def proton_lithium_name_resolver(reaction_raw_name):
     NAMES = [
-        PLI6_NAME,
         "pLi6",
-        "p+Li6",
-        "p+6Li",
-        "p+Li6→α+³He",
-        "p+Li6→³He+α",
-        "p+Li6→³He+⁴He",
-        "p+Li6→⁴He+³He",
-        "Li6+p→α+³He",
-        "Li6+p→³He+α",
-        "Li6+p→³He+⁴He",
-        "Li6+p→⁴He+³He",
-        "p+6Li→α+³He",
-        "p+6Li→³He+α",
-        "p+6Li→³He+⁴He",
-        "p+6Li→⁴He+³He",
     ]
 
     reaction_name = reaction_name_simplify(reaction_raw_name)
@@ -279,7 +528,7 @@ def proton_lithium_name_resolver(reaction_raw_name):
 # this could be re-worked to use a dict so that you don't need to loop over all
 # the entries in sequence.
 def bosch_name_resolver(reaction_raw_name: str):
-    DT_NAMES = [DT_NAME, "DT", "D+T", "D+T→n+α", "D+T→α+n"]
+    DT_NAMES = [DT_NAME, "DT"]
 
     DHE3_NAMES = [
         DHE3_NAME,
@@ -287,18 +536,6 @@ def bosch_name_resolver(reaction_raw_name: str):
         "D3He",
         "D+3He",
         "DHe3",
-        "D+³He→p+⁴He",
-        "D+³He→⁴He+p",
-        "D+³He→p+α",
-        "D+³He→α+p",
-    ]
-
-    DDT_NAMES = [
-        DDT_NAME,
-        "D+D→p+T",
-        "D+D→T+p",
-        "²H+²H→³H+¹H",
-        "²H+²H→¹H+³H",
     ]
 
     DDHE3_NAMES = [
@@ -312,7 +549,7 @@ def bosch_name_resolver(reaction_raw_name: str):
 
     reaction_name = reaction_name_simplify(reaction_raw_name)
 
-    for name_collection in [DT_NAMES, DHE3_NAMES, DDHE3_NAMES, DDT_NAMES]:
+    for name_collection in [DT_NAMES, DHE3_NAMES, DDHE3_NAMES]:
         for n in name_collection:
             if reaction_name == reaction_name_simplify(n):
                 return name_collection[0]
@@ -320,6 +557,12 @@ def bosch_name_resolver(reaction_raw_name: str):
     raise ValueError(
         f"""
         In the bosch_name_resolver, {reaction_raw_name} could not be
-        resolved. Possible options are {DT_NAMES}, {DHE3_NAMES}, {DDT_NAMES},
+        resolved. Possible options are {DT_NAMES}, {DHE3_NAMES},
         {DDHE3_NAMES}."""
     )
+
+
+if __name__ == "__main__":
+    from icecream import ic
+
+    ic(name_resolver("DT"))
